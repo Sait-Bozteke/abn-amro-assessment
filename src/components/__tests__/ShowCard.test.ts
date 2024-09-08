95fa365d70a50a10339109f727ab9bcfab71bc24
Sait-Boz <sait.bozteke63@gmail.com>
2024-09-08 20:30:47 +0200

    added snapshot

--- src/components/__tests__/ShowCard.test.ts
+++ src/components/__tests__/ShowCard.test.ts
@@ -1,31 +1,43 @@
-import { mount } from '@vue/test-utils';
-import ShowCard from '../ShowCard.vue';
+import { shallowMount, RouterLinkStub } from '@vue/test-utils';
+import ShowCard from '@/components/ShowCard.vue';
 import { Show } from '../../types/Show';
 
-const routerLinkStub = {
-  template: '<a><slot /></a>',
-  props: ['to'],
+const show: Show = {
+  id: 1,
+  name: 'Test Show',
+  image: {
+    original: 'http://example.com/image.jpg',
+    medium: '',
+  },
+  genres: ['Drama', 'Action'],
+  rating: { average: 7.5 },
+  summary: 'This is a test summary.',
 };
 
 describe('ShowCard.vue', () => {
-  it('renders props.show correctly', () => {
-    const show: Show = {
-      id: 1,
-      name: 'Test Show',
-      image: {
-        original: 'http://example.com/image.jpg',
-        medium: '',
+  it('renders correctly and matches the snapshot', () => {
+    const wrapper = shallowMount(ShowCard, {
+      props: {
+        show,
       },
-      genres: ['Drama', 'Action'],
-      rating: { average: 7.5 },
-      summary: 'This is a test summary.',
-    };
-
-    const wrapper = mount(ShowCard, {
-      props: { show },
       global: {
-        components: {
-          'router-link': routerLinkStub,
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders props.show correctly', () => {
+    const wrapper = shallowMount(ShowCard, {
+      props: {
+        show,
+      },
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
         },
       },
     });
@@ -35,16 +47,18 @@ describe('ShowCard.vue', () => {
     expect(img.attributes('src')).toBe(show.image.original);
 
     const title = wrapper.find('.card-title');
+    expect(title.exists()).toBe(true);
     expect(title.text()).toBe(show.name);
 
-    const genresText = wrapper.find('.card-text').text();
-    expect(genresText).toContain('Genres: Drama, Action');
+    const genres = wrapper.find('.card-text');
+    expect(genres.exists()).toBe(true);
+    expect(genres.text()).toContain('Genres: Drama, Action');
 
-    const ratingBadge = wrapper.find('span.badge');
-    expect(ratingBadge.text()).toBe(show.rating.average.toString());
-    expect(ratingBadge.classes()).toContain('bg-warning');
+    const rating = wrapper.find('.badge');
+    expect(rating.exists()).toBe(true);
+    expect(rating.text()).toBe(show.rating.average.toString());
 
-    const detailsLink = wrapper.findComponent(routerLinkStub);
+    const detailsLink = wrapper.findComponent(RouterLinkStub);
     expect(detailsLink.exists()).toBe(true);
     expect(detailsLink.props('to')).toEqual({
       name: 'ShowDetails',
